3e91dd55d08e8babff07080ba81488d51fc49235
Jason Christman <jc_84moss@hotmail.com>
2017-01-11 10:39:42 -0700

    Added Shared Kernal Base

---
+++ MarketSolutions/MarketSolutions.SharedKernal/EntityBase.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketSolutions.SharedKernal
+{
+    public abstract class EntityBase<IdType> : IEquatable<EntityBase<IdType>>
+    {
+        private readonly IdType _id;
+
+        public EntityBase(IdType id)
+        {
+            this._id = id;
+        }
+
+        public IdType Id
+        {
+            get { return _id; }
+        }
+
+        public override bool Equals(object entity)
+        {
+            return entity != null
+               && entity is EntityBase<IdType>
+               && this == (EntityBase<IdType>)entity;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+
+        public static bool operator ==(EntityBase<IdType> entity1, EntityBase<IdType> entity2)
+        {
+            if ((object)entity1 == null && (object)entity2 == null)
+            {
+                return true;
+            }
+
+            if ((object)entity1 == null || (object)entity2 == null)
+            {
+                return false;
+            }
+
+            if (entity1.Id.ToString() == entity2.Id.ToString())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(EntityBase<IdType> entity1, EntityBase<IdType> entity2)
+        {
+            return (!(entity1 == entity2));
+        }
+
+        public bool Equals(EntityBase<IdType> other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            return this.Id.Equals(other.Id);
+        }
+    }
+}
